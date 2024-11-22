4e448caa8f5254170903e00713881373f950f022
Flore Perche <flore.perche@gmail.com>
2024-11-22 15:13:42 -0300

    feat: improving test page style

--- lib/first_phx_app_web/live/test_live.ex
+++ lib/first_phx_app_web/live/test_live.ex
@@ -10,27 +10,39 @@ defmodule FirstPhxAppWeb.TestLive do
     #render
     def render (assigns) do
         ~H"""
-        <h1 class="mb-8 text-4xl font-bold text-center">Front Porch Light</h1>
-        <div id="light">
-            <div class="w-full h-6 bg-gray-200 rounded mb-4">
-                <div class="h-6 bg-yellow-300 rounded " style={"width: #{@brightness}%"}>
-                    <%= @brightness %>%
-                </div>
+        <div class="bg-background px-16 py-8 rounded-2xl text-white flex flex-col gap-8 font-zendots text-center font-light">
+
+            <h1 class="text-3xl mb-2">Front Porch Light</h1>
+        
+            <div class="flex items-center justify-between gap-8 h-10">
+                    <div class="w-full bg-grey rounded-3xl relative">
+                        <div style={"width: #{@brightness}%"} 
+                        class="transition-all duration-300 ease-in-and-out h-10 bg-gradient-to-r from-primary to-secondary rounded-3xl blur-lg absolute"/>
+                        <div class="w-full  rounded-3xl overflow-hidden ">                        
+                            <div style={"width: #{@brightness}%"} class="transition-all duration-300 ease-in-and-out h-10 bg-gradient-to-r from-primary to-secondary rounded-3xl" />
+                        </div>
+                    </div>
+                    <div class="flex w-28 justify-center items-center relative rounded-3xl">
+                        <div style={"opacity: #{@brightness}%"} class="transition-all duration-300 ease-in-and-out w-24 h-full bg-gradient-to-r from-primary to-secondary absolute blur-lg rounded-3xl"></div>
+                        <p class="z-10 w-24 bg-background rounded-3xl px-4 py-2 text-lg"><%= @brightness %>%</p>
+                    </div>
+                    
             </div>
+            
             <div class="flex justify-center gap-4">  
-            <button class="bg-gray-200 hover:bg-grey-300 py-2 px-4 rounded" phx-click='off'>
-            <img src="/images/lightbulb-outline.svg" />
-            </button>
-            <button class="bg-gray-200 hover:bg-grey-300 py-2 px-4 rounded" phx-click='down'>
-            <img src="/images/down.svg" />
-            </button>
-            <button class="bg-gray-200 hover:bg-grey-300 py-2 px-4 rounded" phx-click='up'>
-            <img src="/images/up.svg" />
-            </button>
-            <button class="bg-gray-200 hover:bg-grey-300 py-2 px-4 rounded" phx-click='on'>
-            <img src="/images/lightbulb.svg" />
-            </button></div>
-          
+                    <button class="border-2 border-white hover:bg-grey rounded-lg p-4 text-center disabled:opacity-50 disabled:cursor-not-allowed" disabled={@brightness == 0} phx-click='off'>
+                        <img src="/images/lightbulb-outline.svg" />
+                    </button>
+                    <button class="border-2 border-white hover:bg-grey rounded-lg p-4 text-center disabled:opacity-50 disabled:cursor-not-allowed" disabled={@brightness == 0} phx-click='down'>
+                        <img src="/images/down.svg" />
+                    </button>
+                    <button class="border-2 border-white hover:bg-grey rounded-lg p-4 text-center disabled:opacity-50 disabled:cursor-not-allowed" disabled={@brightness == 100} phx-click='up'>
+                        <img src="/images/up.svg" />
+                    </button>
+                    <button class="border-2 border-white hover:bg-grey rounded-lg p-4 text-center disabled:opacity-50 disabled:cursor-not-allowed" disabled={@brightness == 100} phx-click='on'>
+                        <img src="/images/lightbulb.svg" />
+                    </button>
+            </div>
         </div>
         """
     end
